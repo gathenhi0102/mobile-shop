cb719b7c07108124ad88006e620cbe6fb72d2f8e
Nguyen Van Long <nguyenvanlong@gogojungle.vn>
2018-12-23 16:08:00 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-		URL::forceScheme('https');
+		// URL::forceScheme('https');
         //
         view()->composer('header',function($view){
             $trademark = TradeMark::all();
